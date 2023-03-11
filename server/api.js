98f6ed3cc25a9b0c3542fb734489cb811574b680
Zeynab <zeynab11mousavi@gmail.com>
2023-03-11 15:43:29 +0330

    go back to static domaining

--- server/api.js
+++ server/api.js
@@ -1,6 +1,6 @@
 export const URL = "https://ahtback-u095.onrender.com";
 export const api = "https://ahtback-u095.onrender.com/";
-export const site = window.location.hostname;
+export const site = "https://ahtfoods.netlify.app/";
 export const path = `https://ahtback-u095.onrender.com/files/`;
 export const category = "category";
 export const subcategory = "subcategory";
