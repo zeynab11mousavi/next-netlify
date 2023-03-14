a005c8a5e3e306fdbc1d6e4a82075142b0bfd194
Zeynab <zeynab11mousavi@gmail.com>
2023-03-14 10:02:16 +0330

    change catalogue style

--- assets/catalogue/index.js
+++ assets/catalogue/index.js
@@ -1,11 +1,11 @@
 const catalogue = {
-  one: require("./one.jpg"),
-  two: require("./two.jpg"),
-  three: require("./three.jpg"),
-  four: require("./four.jpg"),
-  five: require("./five.jpg"),
-  six: require("./six.jpg"),
-  seven: require("./seven.jpg"),
-  eight: require("./eight.jpg"),
+  one: require('./one.jpg'),
+  two: require('./two.jpg'),
+  three: require('./three.jpg'),
+  four: require('./four.jpg'),
+  five: require('./five.jpg'),
+  six: require('./six.jpg'),
+  seven: require('./seven.jpg'),
+  eight: require('./eight.jpg'),
 };
 export default catalogue;
