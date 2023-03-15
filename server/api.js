3020ea24b6bcee9887d0049ad21503b53ca68d60
Zeynab <zeynab11mousavi@gmail.com>
2023-03-15 11:05:13 +0330

    handle side nav title, rasins and dates

--- server/api.js
+++ server/api.js
@@ -1,7 +1,7 @@
-export const site = `http://ahtfoods.com/`;
-export const URL = "http://localhost:3001";
-export const api = "http://localhost:3001/";
-export const path = `http://localhost:3001/files/`;
+export const URL = 'https://nodejs-873lch.chbk.run'
+export const api = 'https://nodejs-873lch.chbk.run/';
+export const path = `https://nodejs-873lch.chbk.run/files/`;
+export const site = 'https://ahtfoods.com/';
 export const category = "category";
 export const subcategory = "subcategory";
 export const products = "products";
