3dcc1de4a49b79f1d891e042c511e19f3f4d738f
Zeynab <zeynab11mousavi@gmail.com>
2023-04-03 14:38:14 +0430

    handle slider resoulution

--- assets/desktopSlider/index.js
+++ assets/desktopSlider/index.js
@@ -1,8 +1,7 @@
 const desktopSlider = {
     AHTD: require('./AHTD.jpg'),
     FigsD: require('./FigsD.jpg'),
-    PistaD: require('./PistachiosD.jpg'),
-    happy: require('./happy.jpg')
+    PistaD: require('./PistachiosD.jpg')
 }
 
 
--- assets/phoneSlider/index.js
+++
@@ -1,8 +0,0 @@
-const phoneSlider = {
-    AHT: require('./AHT.jpg'),
-    Figs: require('./Figs.jpg'),
-    Pista: require('./Pistachios.jpg')
-}
-
-
-export default phoneSlider;
